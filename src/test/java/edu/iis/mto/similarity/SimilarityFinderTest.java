0e7184428d371d683921e07db62fcfa2df7b668c
209450 <209450@edu.p.lodz.pl>
2019-04-24 15:24:30 +0200

    Test - bothSequencesAreEmptyReturnOne

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -2,16 +2,27 @@ package edu.iis.mto.similarity;
 
 import edu.iis.mto.search.SequenceSearcher;
 import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
 
     private SimilarityFinder similarityFinder;
     private SequenceSearcher sequenceSearcher;
+    private int[] seq1;
+    private int[] seq2;
 
     @Before public void setUp() throws Exception {
         sequenceSearcher = new SequenceSearcherDoubler();
         similarityFinder = new SimilarityFinder(sequenceSearcher);
+    }
+
+    @Test public void bothSequencesAreEmptyReturnOne() {
+        seq1 = new int[]{};
+        seq2 = new int[]{};
+
+        assertThat(similarityFinder.calculateJackardSimilarity(seq1,seq2),is(1.0d));
     }
 }
