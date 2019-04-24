c604aca76ff917133dd09d7afa0637694ff32f5d
209450 <209450@edu.p.lodz.pl>
2019-04-24 14:41:22 +0200

    Utworzenie klasy do testowania SimilarityFinder

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.similarity;
+
+import edu.iis.mto.search.SequenceSearcher;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SimilarityFinderTest {
+
+    private SimilarityFinder similarityFinder;
+
+    @Before public void setUp() throws Exception {
+
+    }
+}
